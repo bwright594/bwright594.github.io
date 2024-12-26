aaf8c4876cde5301d901ee8822776db80090be93
Bennett Wright <bwright594@gmail.com>
2024-12-26 13:36:31 -0600

    make it vite and react

--- Banana.js
+++
@@ -1,7 +0,0 @@
-
-function RandomNumber(x){
-    var y = Math.floor(Math.random() * x.length);
-    document.getElementById("demo").innerHTML = x[y];
-}
-words = ["Fantastic", "Awesome", "Marvelous", "Fabulous", "Rad", "Cool", "The Cooliest", "Fantabulous", "Wonderful", "Excellent", "Spectacular", "Awesome Sauce", ]
-words.push("lame", "Banana", "Gnarly", "Sick Nasty", "Tubular", "A1", )
